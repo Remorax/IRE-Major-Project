07a95009a86a0fcab7c81c2aec0623af357ab0ef
Aman Joshi <amanjoshi668@gamil.com>
2019-11-12 00:03:27 +0530

    onto viewer with twitter login and multiple login capability

--- OntoViewer/src/onto_app/aggregate.py
+++ OntoViewer/src/onto_app/aggregate.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 
 import subprocess, os
 from rdflib.namespace import OWL, RDF, RDFS
-
+import xml.dom.minidom
 RESTRICTIONS = "Restriction_removal-1.0-SNAPSHOT-jar-with-dependencies.jar"
 CLASSES = "Class_removal-1.0-SNAPSHOT-jar-with-dependencies.jar"
 SUBCLASSES = "Subclass_removal-1.0-SNAPSHOT-jar-with-dependencies.jar"
@@ -31,26 +31,29 @@ def generate_final_ontology(name):
             onto_id = res[0]
         else:
             raise ValueError
-        # print (onto_id, RDFS.subClassOf)
+        print ("ontoId:",onto_id)
         owl_path = './data/final/' + name + '.owl'
         if not os.path.isfile(owl_path):
             try:
                 subprocess.run(['cp', './data/owl/' + name + '.owl', owl_path])
             except:
                 raise RuntimeError
-
+        
         result = c.execute("""SELECT * FROM class_relations WHERE onto_id = ?""", (onto_id,))
         relations = result.fetchall()
-        # print ("**",relations, "**")
+        print ("**",relations, "**")
         for r in relations:
             # print (r)
             result = c.execute("""SELECT * FROM class_decisions WHERE relation_id = ?""", (r[0],))
             decisions = result.fetchall()
+            print("Yoda")
             if decisions:
                 print (type(decisions), type(decisions[0]))
+                print("Double YOda")
                 if not accepted([d['approved'] for d in decisions]):
                     print(type(r['quantifier']))
                     print(type(RDFS.subClassOf))
+                    print("Triple Yoda")
                     if r['quantifier'] == str(RDFS.subClassOf):
                         try:
                             subprocess.run(['java', '-jar', SUBCLASSES, r['domain'], r['range'], owl_path])
@@ -58,29 +61,62 @@ def generate_final_ontology(name):
                             raise RuntimeError
                     else:
                         try:
+                            print("Quadrauple Yoda")
                             subprocess.run(['java', '-jar', RESTRICTIONS, r['domain'], r['property'], r['range'], owl_path])
                         except:
                             raise RuntimeError
                 c.execute("""DELETE FROM class_relations WHERE id = ?""", r['id'])
         
-        result = c.execute("""SELECT * FROM nodes WHERE onto_id = ?""", (onto_id,))
-        nodes = result.fetchall()
-        print (nodes)
-        for n in nodes:
-            print (n)
-            result = c.execute("""SELECT * FROM node_decisions WHERE node_id = ?""", n['id'])
-            decisions = result.fetchall()
-            if decisions:
-                if not accepted([d['approved'] for d in decisions]):
-                    try:
-                        subprocess.run(['java', '-jar', CLASSES, n['name'], owl_path])
-                    except:
-                        raise RuntimeError
-                c.execute("""DELETE FROM nodes WHERE id = ?""", n['id'])
+        
+
+        # print ("Results",results)
+        # result = c.execute("""SELECT * FROM nodes WHERE onto_id = ?""", (onto_id,))
+        # nodes = result.fetchall()
+        # for n in nodes:
+        #     print (n)
+        #     result = c.execute("""SELECT * FROM node_decisions WHERE node_id = ?""", n['id'])
+        #     decisions = result.fetchall()
+        #     print(decisions)
+        #     if decisions:
+        #         if not accepted([d['approved'] for d in decisions]):
+        #             print("XYZ",n['name'])
+        #             try:
+        #                 subprocess.run(['java', '-jar', CLASSES, n['name'], owl_path])
+        #             except:
+        #                 raise RuntimeError
+        #         c.execute("""DELETE FROM nodes WHERE id = ?""", n['id'])
         
         trans.commit()
     except:
         trans.rollback()
         raise
+
+    # print("Done")
+    # result = c.execute("""SELECT * FROM nodes WHERE onto_id = ?""", (onto_id,))
+    # nodes = result.fetchall()
+    # print (nodes)
+    # print("/home/harish/Documents/onto/Ontology/OntologyDeterminer/Ontology-Determiner/SSAD22/src/onto_app/data/final/" + name + '.owl')
+    # thefile=open("/home/harish/Documents/onto/Ontology/OntologyDeterminer/Ontology-Determiner/SSAD22/src/onto_app/data/final/" + name + '.owl',"r")
+    # #print(thefile.readlines())
+    # # for line in thefile.readlines():
+    # #     print(line)
+    # outputfile=open("/home/harish/Documents/onto/Ontology/OntologyDeterminer/Ontology-Determiner/SSAD22/src/onto_app/data/final/pizzafinal.owl","w+")
+    # doc = xml.dom.minidom.parse(thefile)
+    # for n in nodes:
+    #     print(n)
+    #     result = c.execute("""SELECT * FROM node_decisions WHERE node_id = ?""", n['id'])
+    #     decisions = result.fetchall()
+    #     print(decisions)
+    #     if decisions:
+    #         if not accepted([d['approved'] for d in decisions]):
+    #             print("XYZ",n['name'])
+    #             nodes = doc.getElementsByTagName(n['name'])
+
+    #             for node in nodes:
+    #                 parent = node.parentNode
+    #                 parent.removeChild(node) 
+    # doc.writexml(outputfile)
+    # outputfile.close()
+    # thefile.close()
     
     
